31fe33fd2939022de25febf2a161c6b87060dfe7
aryanmega12 <106868248+aryanmega12@users.noreply.github.com>
2022-06-11 04:46:23 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ read -p " Does you want to install aryan-mega-trn Shell in Termux?(Yes/No) : " i
 
 if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
     clear
-    cp bash.bashrc $HOME
+    cp bash.mega $HOME
     cd /data/data/com.termux/files/usr/etc
     rm -rf motd
-    rm -rf bash.bashrc
+    rm -rf bash.mega
     cd $HOME
-    mv bash.bashrc /data/data/com.termux/files/usr/etc
+    mv bash.mega /data/data/com.termux/files/usr/etc
     echo -e "\e[1;91mInstall Shod dadash"
     echo -e "Restart Termux"
     exit  3
